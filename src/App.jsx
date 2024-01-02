e1ce166562afc1e739521be52bc8a22722fa146c
Dilip Nayak <dilipnayak7652@gmail.com>
2024-01-02 14:44:43 +0530

    blog

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_REACT_APPWRITE_URL)
+  return (
+    <>
+      <h1>Blog Website with AppWrite</h1>
+    </>
+  )
+}
+
+export default App
