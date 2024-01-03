b0af434d28f4d9859f50e14d2d62e0a867cf136f
Dilip Nayak <dilipnayak7652@gmail.com>
2024-01-03 18:01:55 +0530

    blog

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,7 @@ import { useDispatch } from 'react-redux'
 import './App.css'
 import authService from './appwrite/auth'
 import { login, logout } from './store/authSlice'
-import Header from './components/header/header'
-import Footer from './components/footer/Footer'
+import { Header, Footer } from './components'
 import { Outlet } from 'react-router-dom'
 
 function App() {
---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import Header from "./Header/Header";
+import Footer from "./footer/Footer";
+import Container from "postcss/lib/container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+
+export {
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
+}
